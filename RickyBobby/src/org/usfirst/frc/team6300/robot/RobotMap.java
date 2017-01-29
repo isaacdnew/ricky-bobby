e24f164dc2400cdff870bf8e878ea1f839cb2fd1
isaac <isaac@eyelow>
2017-01-28 21:36:24 -0500

    initial commit

---
+++ RickyBobby/src/org/usfirst/frc/team6300/robot/RobotMap.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team6300.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	//oi
+	public static int gamepadDr = 0;
+	public static int forwardAxis = 1;
+	public static int slideAxis = 0;
+	public static int rotateAxis = 4;
+	public static int throttleAxis = 3;
+	
+	//drivetrain
+	public static int lfMotor = 0;
+	public static int rfMotor = 1;
+	public static int lbMotor = 2;
+	public static int rbMotor = 3;
+}
---
+++ RickyBobby/src/org/usfirst/frc/team6300/robot/subsystems/Drivetrain.java
@@ -0,0 +1,93 @@
+package org.usfirst.frc.team6300.robot.subsystems;
+
+import org.usfirst.frc.team6300.robot.RobotMap;
+import org.usfirst.frc.team6300.robot.commands.MecanumDrive;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * The drivetrain, consisting of four drive motors.
+ */
+public class Drivetrain extends Subsystem {
+	//motors:
+	static SpeedController lfMotor = new VictorSP(RobotMap.lfMotor);
+	static SpeedController rfMotor = new VictorSP(RobotMap.rfMotor);
+	static SpeedController lbMotor = new VictorSP(RobotMap.lbMotor);
+	static SpeedController rbMotor = new VictorSP(RobotMap.rbMotor);
+	//motor speeds:
+	double lfSpeed = 0;
+	double rfSpeed = 0;
+	double lbSpeed = 0;
+	double rbSpeed = 0;
+	
+	/**
+	 * Drives the robot with a joystick.
+	 * @param joy The joystick to use
+	 * @param forwardAxis The axis that, when positive, moves the robot forward
+	 * @param slideAxis The axis that, when positive, slides the robot to its right
+	 * @param rotationAxis The axis that, when positive, turns the robot clockwise
+	 * @param throttleAxis The axis that, when positive, increases the power coefficient
+	 * @param minThrottle The minimum throttle that the robot can go at
+	 */
+	public void teleDrive(Joystick joy, int forwardAxis, int slideAxis, int rotateAxis, int throttleAxis, double minThrottle) {
+		double forwardSpeed = joy.getRawAxis(forwardAxis);
+		double slideSpeed = joy.getRawAxis(slideAxis);
+		double rotateSpeed = joy.getRawAxis(rotateAxis);
+		double throttle = joy.getRawAxis(throttleAxis);
+		
+		//set power coefficient
+		double power = throttle + minThrottle;
+		if (power > 1) {power = 1;}
+		else if (power < -1) {power = -1;}
+		power /= 3;
+		
+		
+		//forward
+		lfSpeed = forwardSpeed * power;
+		rfSpeed = forwardSpeed * power;
+		lbSpeed = forwardSpeed * power;
+		rbSpeed = forwardSpeed * power;
+		
+		//slide
+		lfSpeed -= slideSpeed * power;
+		rfSpeed += slideSpeed * power;
+		lbSpeed += slideSpeed * power;
+		rbSpeed -= slideSpeed * power;
+		
+		//rotate
+		lfSpeed += rotateSpeed * power;
+		rfSpeed -= rotateSpeed * power;
+		lbSpeed += rotateSpeed * power;
+		rbSpeed -= rotateSpeed * power;
+		
+		updateMotors();
+	}
+	
+	public void updateMotors() {
+		lfMotor.set(lfSpeed);
+		rfMotor.set(rfSpeed);
+		lbMotor.set(lbSpeed);
+		rbMotor.set(rbSpeed);
+	}
+	
+	public void stop() {
+		lfMotor.stopMotor();
+		rfMotor.stopMotor();
+		lbMotor.stopMotor();
+		rbMotor.stopMotor();
+	}
+	
+	public void coast() {
+		lfMotor.set(0);
+		rfMotor.set(0);
+		lbMotor.set(0);
+		rbMotor.set(0);
+	}
+	
+	public void initDefaultCommand() {
+		setDefaultCommand(new MecanumDrive());
+	}
+}
