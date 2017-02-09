d1b23e4f29489b18fbfe65bd8a7c0d8b6e946b45
robotics <robotics@northwoodschool.com>
2017-02-09 14:24:21 -0500

    Agitators Work!

--- RickyBobby/src/org/usfirst/frc/team6300/robot/commands/TeleAgitator.java
+++ RickyBobby/src/org/usfirst/frc/team6300/robot/commands/TeleAgitator.java
@@ -29,12 +29,12 @@ public class TeleAgitator extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.shooter.stop();
+    	Robot.agitator.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.shooter.stop();
+    	Robot.agitator.stop();
     }
 }
