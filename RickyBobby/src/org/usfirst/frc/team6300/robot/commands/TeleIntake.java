3538af31db23f0d65f48c31b68aba4238e580306
isaac <isaac@eyelow>
2017-02-18 19:36:21 -0500

    added a deadzone for the drive joystick, and moved OI numbers from RobotMap to OI.

--- RickyBobby/src/org/usfirst/frc/team6300/robot/commands/TeleIntake.java
+++ RickyBobby/src/org/usfirst/frc/team6300/robot/commands/TeleIntake.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team6300.robot.commands;
 
 import org.usfirst.frc.team6300.robot.OI;
 import org.usfirst.frc.team6300.robot.Robot;
-import org.usfirst.frc.team6300.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -17,12 +16,11 @@ public class TeleIntake extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	System.out.println("Initializing the intake.");
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.intake.setSpeed(OI.gamepadSh.getRawAxis(RobotMap.pullAxis) - OI.gamepadSh.getRawAxis(RobotMap.pushAxis));
+    	Robot.intake.setSpeed(OI.gamepadSh.getRawAxis(OI.rightTrigger) - OI.gamepadSh.getRawAxis(OI.leftTrigger));
     }
 
     // Make this return true when this Command no longer needs to run execute()
